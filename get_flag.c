1b0e51a9c3710ded7b1c139d0c5031624f7f8f8f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-17 08:46:23 +0100

    Update get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -1,33 +1,30 @@
 #include "main.h"
 
 /**
- * get_flag - turns on flags if _printf finds
- * a flag modifier in the format string
- * @s: character that holds the flag specifier
- * @f: pointer to the struct flags in which we turn the flags on
- *
- * Return: 1 if a flag has been turned on, 0 otherwise
- * Authors: Ehoneah Obed & Abdulhakeem Badejo
+ * get_flag - flag modifier for formated string
+ * @s: char flag
+ * @f: struct pointer flag
+ * Return: 1 if valid flag, else (0)
  */
 int get_flag(char s, flags_t *f)
 {
-	int i = 0;
+	int value = 0;
 
 	switch (s)
 	{
 		case '+':
 			f->plus = 1;
-			i = 1;
+			value = 1;
 			break;
 		case ' ':
 			f->space = 1;
-			i = 1;
+			value = 1;
 			break;
 		case '#':
 			f->hash = 1;
-			i = 1;
+			value = 1;
 			break;
 	}
 
-	return (i);
+	return (value);
 }
