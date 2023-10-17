cdeda8a01122842c6e2ea5ac5559227e6b9d8d56
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-17 21:41:14 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -35,9 +35,9 @@ int _printf(const char *format, ...)
 			flags = get_flags(format, &i);
 			width = get_width(format, &i, list);
 			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
+			size = pr_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer,
+			printed = fnt_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
