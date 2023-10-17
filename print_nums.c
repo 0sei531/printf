13c2e44451e70d647fb9aeb08e0b4263c8dac807
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-17 09:23:16 +0100

    Update print_nums.c

--- print_nums.c
+++ print_nums.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_int - prints an integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_int - print integer
+ * @l: va_list args
+ * @f: pointer flags
+ * Return: numbers of printed char
  */
 int print_int(va_list l, flags_t *f)
 {
