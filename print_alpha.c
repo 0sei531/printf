09c95e653059081d40ada14c4def615365aaf21f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-17 08:59:10 +0100

    Update print_alpha.c

--- print_alpha.c
+++ print_alpha.c
@@ -1,30 +1,27 @@
 #include "main.h"
 
 /**
- * print_string - loops through a string and prints
- * every character
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_string - loop and print char
+ * @l: va_list args
+ * @f: struct flags
+ * Return: char printed
  */
 int print_string(va_list l, flags_t *f)
 {
-	char *s = va_arg(l, char *);
+	char *str = va_arg(l, char *);
 
 	(void)f;
 
-	if (!s)
-		s = "(null)";
-	return (_puts(s));
+	if (!str)
+		str = "(null)";
+	return (_puts(str));
 }
 
 /**
- * print_char - prints a character
- * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * print_char - print char
+ * @l: va_list args
+ * @f: struct flags
+ * Return: printed char
  */
 int print_char(va_list l, flags_t *f)
 {
