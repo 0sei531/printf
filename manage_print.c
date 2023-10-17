eecf492f540498b5114018203505bc57dd405121
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-18 00:36:26 +0100

    Update manage_print.c

--- manage_print.c
+++ manage_print.c
@@ -17,7 +17,7 @@ int fnt_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', pr_char}, {'s', pr_str}, {'%', pr_percent},
-		{'i', print_int}, {'d', print_int}, {'b', pr_bin},
+		{'i', pr_int}, {'d', pr_int}, {'b', pr_bin},
 		{'u', pr_unsigned}, {'o', pr_octal}, {'x', pr_hexadex},
 		{'X', pr_hex_upper}, {'p', pr_ptr}, {'S', pr_unprintable},
 		{'r', pr_rev}, {'R', pr_rot13str}, {'\0', NULL}
