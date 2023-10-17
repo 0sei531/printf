54872f3f813400e33fdfcf31f6e68a221c94b31f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-17 20:35:07 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
+ * is_printable -  printable
+ * @c: Char 
  *
- * Return: 1 if c is printable, 0 otherwise
+ * Return: 1, else 0
  */
-int is_printable(char c)
+int printable(char c)
 {
 	if (c >= 32 && c < 127)
 		return (1);
@@ -21,7 +21,7 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int i)
+int app_hexa(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
@@ -43,7 +43,7 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
-int is_digit(char c)
+int pr_digit(char c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
@@ -58,7 +58,7 @@ int is_digit(char c)
  *
  * Return: Casted value of num
  */
-long int convert_size_number(long int num, int size)
+long int pr_size_num(long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
@@ -75,7 +75,7 @@ long int convert_size_number(long int num, int size)
  *
  * Return: Casted value of num
  */
-long int convert_size_unsgnd(unsigned long int num, int size)
+long int pr_size_unsgnd(unsigned long int num, int size)
 {
 	if (size == S_LONG)
 		return (num);
