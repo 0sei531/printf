afc0e98282d03bc76dca7c0eefdafddb12b2226c
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2023-10-17 09:45:18 +0100

    Update get_flag.c

--- get_flag.c
+++ get_flag.c
@@ -1,30 +1,30 @@
 #include "main.h"
 
 /**
- * get_flag - flag modifier for formated string
- * @s: char flag
- * @f: struct pointer flag
+ * get_flag - _printf flag
+ * @s: specifier character
+ * @f: pointer structure
  * Return: 1 if valid flag, else (0)
  */
 int get_flag(char s, flags_t *f)
 {
-	int value = 0;
+	int i = 0;
 
 	switch (s)
 	{
 		case '+':
 			f->plus = 1;
-			value = 1;
+			i = 1;
 			break;
 		case ' ':
 			f->space = 1;
-			value = 1;
+			i = 1;
 			break;
 		case '#':
 			f->hash = 1;
-			value = 1;
+			i = 1;
 			break;
 	}
 
-	return (value);
+	return (i);
 }
